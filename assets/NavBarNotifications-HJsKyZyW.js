aea126810db48da1999a0085f5069821272bffa5
Paul <5923407+pamls@users.noreply.github.com>
2026-02-10 23:32:52 -0500

    new release

---
+++ assets/NavBarNotifications-HJsKyZyW.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NavBarNotifications.vue_vue_type_script_setup_true_lang-d10SZXho.js";import"./index-DYtKiG3f.js";export{m as default};
---
+++ assets/NavbarShortcuts-DChQ-MCA.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NavbarShortcuts.vue_vue_type_script_setup_true_lang-D5ldeSZN.js";import"./index-DYtKiG3f.js";export{m as default};
